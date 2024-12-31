6a4083beeae9173dfb02b2110626b3c1d9f29d3a
Duszke333 <duszke333@gmail.com>
2024-12-31 16:02:47 +0100

    Improved readability

--- src/knapsack/typing.py
+++ src/knapsack/typing.py
@@ -1,10 +1,23 @@
 class Item:
+    """
+    Class to represent an item in the knapsack problem.
+    # Fields:
+    value (`int | float`) - the value of the item. \\
+    weight (`int | float`) - the weight of the item. \\
+    ratio (`float`) - the value-to-weight ratio of the item. \\
+    picked (`bool | None`) - whether the item is picked or not. \\
+    """
+
     value: int | float
     weight: int | float
     ratio: float
     picked: bool | None
+
     def __init__(self, value, weight):
         self.value = value
         self.weight = weight
         self.ratio = value / weight
         self.picked = None
+
+    def __repr__(self):
+        return f"Item(value={self.value}, weight={self.weight}, ratio={self.ratio}, picked={self.picked})"
