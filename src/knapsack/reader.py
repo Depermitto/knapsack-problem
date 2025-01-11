3cd9daaaf3da2054fcb3edda3911967897e8d4bc
Duszke333 <duszke333@gmail.com>
2025-01-11 13:31:32 +0100

    Now reading normal types, not numpy

--- src/knapsack/reader.py
+++ src/knapsack/reader.py
@@ -10,11 +10,11 @@ def read_data(file_path: str) -> tuple[int | float, int | float, list[Item]]:
     # Returns:
         result (`tuple[int | float, int | float, list[Item]]`) - a tuple containing the optimal value, total capacity, and a list of items.
     """
-    df = pd.read_csv(file_path)
+    df = pd.read_csv(file_path).astype('object')
     optimal_value = df.iloc[0, 0]
     total_capacity = df.iloc[0, 1]
     items = []
     for i in range(1, len(df)):
         item = Item(df.iloc[i, 0], df.iloc[i, 1])
         items.append(item)
-    return optimal_value, total_capacity, items
+    return optimal_value, total_capacity, items # type: ignore
